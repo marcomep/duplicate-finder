21744a84ee6f365a52778ede671258666e79ae92
marcomep <marcomep@gmail.com>
2023-08-24 19:28:40 +0000

    various non tested improvements

--- duplicateFinder.py
+++ duplicateFinder.py
@@ -2,35 +2,34 @@ import hashlib
 import shutil
 import os
 import argparse
-import sys
 import glob
 
-def computeHash(file_name):
+def compute_hash(file_name):
     with open(file_name, "rb") as f:
         data = f.read()
         return hashlib.sha256(data).hexdigest()
 
-def parseArguments():
+def parse_arguments():
     parser = argparse.ArgumentParser(prog="DuplicateFider", description="Find and delete duplicate files.")
-    parser.add_argument("-i", "--input", dest="input", nargs="1", required=True, help="Directory to scan files. It include all subfolder at any depts. Hidden files are excluded.")
-    parser.add_argument("-o", "--output", dest="output", nargs="1", required=True, help="Directory where to move/copy file that must be delete because duplicated.")
+    parser.add_argument("-i", "--input", dest="input", nargs=1, required=True, help="Directory to scan files. It include all subfolder at any depts. Hidden files are excluded.")
+    parser.add_argument("-o", "--output", dest="output", nargs=1, required=True, help="Directory where to move/copy file that must be delete because duplicated.")
     parser.add_argument("-a", "--action", dest="action", nargs="?", required=False, choices=['c', 'm'], default='c', help="Action to do when a duplicate is found: 'c' [Default] for copying file in output directory, 'm' for move.")
     return parser.parse_args()
 
 def main():
     print("Find Duplicates")
-    input_cfg = parseArguments()
+    input_cfg = parse_arguments()
 
     not_duplicate = {}
     duplicates = {}
 
-    file_list = glob.glob(os.path.join(FILES_DIR_DELETED, "**/*"), recursive=True)
+    file_list = glob.glob(os.path.join(input_cfg.input, "**/*"), recursive=True)
     
     # Iterate directory
     for full_name in file_list:
         # check if current file is a file
         if os.path.isfile(full_name):
-            sha_file = computeHash(full_name)
+            sha_file = compute_hash(full_name)
             if sha_file in not_duplicate:
                 duplicates.update({
                     sha_file: {
@@ -40,14 +39,20 @@ def main():
                 })
 
                 # check this code block
-                base_out_dir = input_cfg.output
-                if (not base_out_dir.endswith('/')):
+                base_in_dir = input_cfg.input
+                if (not base_in_dir.endswith('/')):
                     base_dir += "/"
 
-                out_file_clone = os.path.join(FILES_DIR_DELETED, "DELETE_" + file_path)
+                file_name = os.path.basename(full_name)
+                len_base_in_dir = len(base_in_dir)
+                subpath = file_name[len_base_in_dir:len_base_in_dir - len(file_name)]
+                out_file_clone = os.path.join(input_cfg.output, subpath, "DELETE_" + file_name)
                 shutil.copyfile(full_name, out_file_clone)
- 
-                out_file_original = os.path.join(FILES_DIR_DELETED, "ORIGINAL_" + os.path.basename(not_duplicate[sha_file]["path"]))
+
+                file_name = os.path.basename(not_duplicate[sha_file]["path"])
+                len_base_in_dir = len(base_in_dir)
+                subpath = not_duplicate[sha_file]["path"][len_base_in_dir:len_base_in_dir - len(file_name)]
+                out_file_original = os.path.join(input_cfg.output, subpath, "ORIGINAL_" + file_name)
                 shutil.copyfile(not_duplicate[sha_file]["path"], out_file_original)
             else:
                 not_duplicate.update({
@@ -56,7 +61,7 @@ def main():
                     }
                 })
         else:
-            print("Error", file_path, "is a folder!")
+            print("Error", full_name, "is a folder!")
 
     print("End, report\n", duplicates)
 
