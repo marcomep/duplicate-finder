d9441d301d8b0cec53d847adc80bde64d614a8c0
marcomep <marcomep@gmail.com>
2023-08-25 18:55:04 +0200

    various fix

--- duplicateFinder.py
+++ duplicateFinder.py
@@ -3,6 +3,10 @@ import shutil
 import os
 import argparse
 import glob
+import pathlib
+
+def copy_file(from_, to):
+    pass
 
 
 # return directory without root path and file name
@@ -12,7 +16,7 @@ def remove_base_dir(full_file_name, base_dir):
 
     file_name = os.path.basename(full_file_name)
     len_base_dir = len(base_dir)
-    sub_path = full_file_name[len_base_dir:len_base_dir - len(file_name)]
+    sub_path = full_file_name[len_base_dir:len_base_dir + (len(full_file_name) - len(file_name))]
     return sub_path, file_name
 
 
@@ -26,10 +30,10 @@ def parse_arguments():
     parser = argparse.ArgumentParser(prog="DuplicateFider",
                                      description="Find and delete duplicate files. The oldest file is considerate the "
                                                  "original.")
-    parser.add_argument("-i", "--input", dest="input", nargs=1, required=True,
+    parser.add_argument("-i", "--input", dest="input", nargs=1, required=True, type=pathlib.Path,
                         help="Directory to scan files. It include all subfolder at any depts. Hidden files are "
                              "excluded.")
-    parser.add_argument("-o", "--output", dest="output", nargs=1, required=True,
+    parser.add_argument("-o", "--output", dest="output", nargs=1, required=True, type=pathlib.Path,
                         help="Directory where to move/copy file that must be delete because duplicated.")
     parser.add_argument("-a", "--action", dest="action", nargs="?", required=False, choices=['c', 'm'], default='c',
                         help="Action to do when a duplicate is found: 'c' [Default] for copying file in output "
@@ -60,13 +64,14 @@ def main():
                     }
                 })
 
-                base_in_dir = input_cfg.input
+                base_in_dir = str(input_cfg.input[0])
                 sub_path, file_name = remove_base_dir(full_name, base_in_dir)
-                out_file_clone = os.path.join(input_cfg.output, sub_path, "DELETE_" + file_name)
+                print(sub_path)
+                out_file_clone = os.path.join(input_cfg.output[0], sub_path, "DELETE_" + file_name)
                 shutil.copyfile(full_name, out_file_clone)
 
                 sub_path, file_name = remove_base_dir(not_duplicate[sha_file]["path"], base_in_dir)
-                out_file_original = os.path.join(input_cfg.output, sub_path,
+                out_file_original = os.path.join(input_cfg.output[0], sub_path,
                                                  "ORIGINAL_" + file_name)
                 shutil.copyfile(not_duplicate[sha_file]["path"], out_file_original)
             else:
@@ -75,8 +80,6 @@ def main():
                         "path": full_name
                     }
                 })
-        else:
-            print("Error", full_name, "is a folder!")
 
     print("End, report\n", duplicates)
 
